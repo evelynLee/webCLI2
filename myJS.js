3c617c757c1c335645d0e49a01c5943ae2af9178
evelynLee <evelynlee8@gmail.com>
2020-11-30 15:21:08 +0800

    測試第一版
    測試喔

---
+++ myJS.js
@@ -0,0 +1,18 @@
+$(function() {
+
+
+    // $("#dropdown").click(function() {
+    //     $(".header ul").fadeIn(500);
+    // });
+
+    // $("#close").click(function() {
+    //     $(".header ul").fadeOut(500);
+    // });
+
+    $("#dropdown").click(function() {
+        $(".header ul").fadeToggle(500);
+    });
+
+
+
+});
